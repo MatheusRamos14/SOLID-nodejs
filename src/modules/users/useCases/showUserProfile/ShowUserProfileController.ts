0aad3f2144ba2ad8a341b8cf4d9f86acde034e6a
Matheus Ramos <mramos@rootbox.tech>
2023-04-22 18:54:32 -0300

    Show user profile fixed

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,8 +6,13 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    const userProfile = this.showUserProfileUseCase.execute({ user_id });
+    
+    return response.json(userProfile);
   }
+  
 }
 
 export { ShowUserProfileController };
