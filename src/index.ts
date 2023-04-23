0e4bacc712e63e3e4a5b662066670bde436c93af
Matheus Ramos <mramos@rootbox.tech>
2023-04-22 23:57:55 -0300

    Swagger added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import swaggerUI from 'swagger-ui-express';
 
+import swaggerJSON from './swagger.json';
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
-
 app.use(express.json());
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 app.use("/users", usersRoutes);
 
 export { app };
